9a31450bb71526bc89cb74d5c711e567fdaa139d
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-06-26 13:40:22 -0400

    changed innerHTML to append child so that the text in the input will appear as a list item and not just update the existing list item.

--- public/main.js
+++ public/main.js
@@ -2,9 +2,13 @@
 let inputText = document.querySelector('.inputToDo')
 let addButton = document.querySelector('.addToDo')
 
+// Get the delete button from the list item
+let deleteBtn = document.querySelector('.deleteListItem')
+
 // when user clicks on the add button, the text from the input
 // appears in the list below the input area
 addButton.addEventListener('click', addListItem)
+// deleteBtn.addEventListener('click', deleteListItem)
     // the input item in the list will have a button to remove text
     // and a button to edit the text
 
@@ -18,9 +22,16 @@ addButton.addEventListener('click', addListItem)
         let li = document.createElement('li')
         let inputTextValue = inputText.value
         console.log(inputTextValue)
-
+        li.innerHTML = `${inputTextValue}`
         let toDoList = document.querySelector('.toDoList')
 
-        toDoList.innerHTML = `<li>${inputTextValue}</li>`
+        toDoList.appendChild(li)
 
+        inputTextValue = ''
+      }
+
+
+    // Function that removes todo list item
+      function deleteListItem() {
+        console.log('delete btn clicked')
       }
