e1a9acf56f056fc7da215af19e6da36395c2cb86
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-06-26 12:49:18 -0400

    created variables for the text input and the add button

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+// Get the text from the input area
+let inputText = document.querySelector('.inputToDo')
+let addButton = document.querySelector('.addToDo')
 
-document.addEventListener('DOMContentLoaded', main)
+// when user clicks on the add button, the text from the input
+// appears in the list below the input area
+
+    // the input item in the list will have a button to remove text
+    // and a button to edit the text
