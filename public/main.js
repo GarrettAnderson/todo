d666f1c976ee3978910b3de729877de94c31cad7
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-06-26 13:09:03 -0400

    created javascript function to console log the value of the text field when the user clicks on the add button. added createelement to create li.

--- public/main.js
+++ public/main.js
@@ -4,6 +4,17 @@ let addButton = document.querySelector('.addToDo')
 
 // when user clicks on the add button, the text from the input
 // appears in the list below the input area
-
+addButton.addEventListener('click', addListItem)
     // the input item in the list will have a button to remove text
     // and a button to edit the text
+
+function addListItem () {
+  console.log('click add button')
+
+  let li = document.createElement('li')
+
+  console.log(inputText.value)
+}
+    // when click on the add button, run a function that
+    // takes the text content from the input and
+    // adds as an li html element in toDoList
